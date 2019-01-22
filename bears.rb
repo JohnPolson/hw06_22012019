75b6ba3492f50bec77de863fa81646efa6e93d3a
JohnPolson <JP@JPs-MacBook-Pro.local>
2019-01-22 23:43:48 +0000

    Second upload

--- bears.rb
+++ bears.rb
@@ -1,4 +1,4 @@
-class Bear
+class Bears
 
   attr_accessor :bearname, :type, :food
 
@@ -9,9 +9,16 @@ class Bear
 
     end
 
-def get_bear_name
-  return @bearname
-end
+    def get_bear_name()
+      return @bearname
+    end
 
+    def get_bear_type()
+      return @type
+    end
+
+    def get_bear_food()
+      return @food
+    end
 
 end
--- specs/bears_specs.rb
+++ specs/bears_specs.rb
@@ -5,17 +5,32 @@ require_relative("../bears")
 # require_relative("../river")
 # require_relative("../fish")
 
-class BearTest < MiniTest::Test
+class BearsTest < MiniTest::Test
 
   def setup
-    @bear_01 = Bear.new("Yogi", "Grizzly", 30)
-    @bear_02 = Bear.new("BooBoo", "Brown", 5)
-    @bear_03 = Bear.new("LingLing", "Panda", 12)
-    @bear_04 = Bear.new("Leon", "Black", 20)
+    @bear_01 = Bears.new("Yogi", "Grizzly", 30)
+    @bear_02 = Bears.new("BooBoo", "Brown", 5)
+    @bear_03 = Bears.new("LingLing", "Panda", 12)
+    @bear_04 = Bears.new("Leon", "Black", 20)
   end
 
   def test_get_bear_name()
     assert_equal("Yogi", @bear_01.get_bear_name)
   end
+
+  def test_get_bear_type()
+    assert_equal("Panda", @bear_03.get_bear_type)
+  end
+
+  def test_get_bear_food()
+    assert_equal([20], @bear_04.get_bear_food)
+  end
+
+  # def test_print
+  #   return p @bear_03.get_bear_type
+  #   # p @bear_03.get_bear_type
+  #   # p @bear_04.get_bear_food
+  # end
+
 
 end
